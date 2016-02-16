d8381a7572e3aad417e1c87c548f74ca93edf923
echo <goalinternet@gmail.com>
2016-02-16 20:39:54 +0200

    caroptions can be updated from disp

---
+++ src/main/java/tb/apiyandex/SettingsController.java
@@ -0,0 +1,45 @@
+package tb.apiyandex;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import tb.dao.IPartnerDao;
+import tb.domain.Partner;
+import tb.domain.PartnerSettings;
+import tb.service.exceptions.PartnerNotFoundException;
+
+@Controller("apiyandexCarAckController")
+@RequestMapping("/settings")
+public class SettingsController {
+	@Autowired
+	private IPartnerDao partnerDao;
+
+	@RequestMapping(value = "")
+	public void index(@RequestBody String str, HttpServletResponse response, @RequestParam("clid") String clid,
+			@RequestParam("apikey") String apikey) {
+
+		try {
+			Partner partner = partnerDao.getByApiId(clid);
+			if (partner == null) {
+				throw new PartnerNotFoundException(clid);
+			}
+
+			if (!partner.getApiKey().equals(apikey)) {
+				throw new PartnerNotFoundException(clid);
+			}
+
+			PartnerSettings settings = new PartnerSettings();
+			settings.setPartner(partner);
+			settings.setSettings(str);
+			partnerDao.saveSettings(settings);
+
+		} catch (PartnerNotFoundException e) {
+			response.setStatus(403);
+		}
+	}
+}
