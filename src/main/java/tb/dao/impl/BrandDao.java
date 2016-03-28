92b170bd35e8b8de7f4da2ab9346a3309af04034
echo <goalinternet@gmail.com>
2016-03-28 11:39:10 +0300

    fix

--- src/main/java/tb/dao/impl/BrandDao.java
+++ src/main/java/tb/dao/impl/BrandDao.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tb.dao.IBrandDao;
 import tb.domain.Brand;
@@ -14,6 +15,7 @@ public class BrandDao implements IBrandDao {
 	@Autowired
 	private SessionFactory sessionFactory;
 
+	@Transactional
 	@Override
 	public Brand get(String codeName) {
 		return (Brand) sessionFactory.getCurrentSession()
